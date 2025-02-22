ec32270b2472f23fd4eee815ec1df41ddd52b2c0
SymplyMatt <symplymatt@gmail.com>
2025-02-22 22:16:17 +0100

    Update CategoriesSlider spacing, add Recommended section to Home, and replace font files with WOFF format

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,6 +8,21 @@ const Home = () => {
             <Header />
             <HeroSlider />
             <CategoriesSlider />
+            <div className="w-full px-[60px] flex flex-col items-center gap-[24px] mb-[50px]">
+                <div className="w-full flex items-center justify-between h-[48px]">
+                    <div className="flex items-center gap-[32px]">
+                        <div className="text-[24px] font-semibold leading-[32.78px] tracking-[0%]">Recommended</div>
+                        <div className="flex items-center gap-[24px]">
+                            <img src="/src/assets/images/line_sm.svg" />
+                            <div className="h-[43px] bg-[#2B2B2B] text-white flex items-center justify-center p-[12px] text-[14px] font-semibold leading-[19.12px] tracking-[0%]">WOMEN'S</div>
+                            <div className="flex items-center justify-center text-[14px] font-medium leading-[19.12px] tracking-[0%]">MEN'S</div>
+                            <div className="flex items-center justify-center text-[14px] font-medium leading-[19.12px] tracking-[0%]">CHILDREN'S</div>
+                            <div className="flex items-center justify-center text-[14px] font-medium leading-[19.12px] tracking-[0%]">BEAUTY</div>
+                            <div className="flex items-center justify-center text-[14px] font-medium leading-[19.12px] tracking-[0%]">HOME+LIFESTYLE</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
       </div>
   )
 }
