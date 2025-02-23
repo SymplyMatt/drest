e02d2a97c63b3778f9374f444383cb4037f4af40
SymplyMatt <symplymatt@gmail.com>
2025-02-23 02:54:16 +0100

    Refactor GridSlider component for improved layout and hover effects; add new styling for image container

--- src/components/home/GridSlider.tsx
+++ src/components/home/GridSlider.tsx
@@ -28,15 +28,15 @@ const GridSlider = () => {
                 onSwiper={(swiper) => (swiperRef.current = swiper)}
             >
                 {[...Array(100)].map((_, index) => (
-                    <SwiperSlide key={index} className="flex justify-center bg-[#F3F3F3] border border-[#E6E6E6] transition-all duration-1000 hover:scale-[1.05] hover:h-[500px] hover:w-[375px] hover:z-10 relative">
-                        <div className="w-full h-full flex flex-col justify-center gap-[14px] bg-[#F3F3F3] p-[20px] transition-all duration-1000 hover:scale-[1.2] hover:h-[500px] hover:w-[375px] hover:z-10 relative">
-                            <div className="relative bg-white w-full h-[80%] flex items-center justify-center">
-                                <img src={`/src/assets/images/arrivals1.png`} className="" alt="Product" />
-                                <div className="absolute bottom-[0px] right-[0px] w-full h-[60px] border-t border-[#D6D6D5] flex items-center justify-between">
-                                    <div className="w-full flex items-center justify-center">Sweat Shirts</div>
-                                    <div className="h-[20.5px] bg-[#D6D6D5] w-[1px]"></div>
-                                    <div className="w-full flex items-center justify-center">840 Products</div>
-                                </div>
+                    <SwiperSlide key={index} className="flex justify-center bg-[#F3F3F3] border border-[#E6E6E6] transition-all duration-1000 hover:h-[500px] hover:w-[375px] hover:z-10 relative px-[20px] py-[60px] hover:px-[10px] hover:py-[20px] hover:scale-[1.05]">
+                        <div className="w-full h-full flex flex-col justify-center transition-all duration-1000 hover:z-10 relative bg-white">
+                            <div className="w-full flex items-center justify-center new_arrivals_img">
+                                <img src={`/src/assets/images/arrivals1.png`} className=""/>
+                            </div>
+                            <div className="w-full h-[60px] border-t border-[#D6D6D5] flex items-center justify-between">
+                                <div className="w-full flex items-center justify-center">Sweat Shirts</div>
+                                <div className="h-[20.5px] bg-[#D6D6D5] w-[1px]"></div>
+                                <div className="w-full flex items-center justify-center">840 Products</div>
                             </div>
                         </div>
                     </SwiperSlide>
