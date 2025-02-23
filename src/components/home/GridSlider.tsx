3bcbde3ecffe5f16c9a715cbb233c8ba53a1ed56
SymplyMatt <symplymatt@gmail.com>
2025-02-23 02:33:59 +0100

    Enhance GridSlider component with new layout and hover effects; update Home component to include New Arrivals section

--- src/components/home/GridSlider.tsx
+++ src/components/home/GridSlider.tsx
@@ -1,31 +1,43 @@
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper as SwiperClass } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import { Autoplay, Navigation } from "swiper/modules";
+import { Autoplay } from "swiper/modules";
+import { useRef, useState } from "react";
 const GridSlider = () => {
+    const swiperRef = useRef<SwiperClass | null>(null);
+    const [slidesPerView, setSlidesPerView] = useState(4);
     return (
-        <div className="px-[60px] w-full slider_arrivals">
+        <div className="w-full slider_arrivals" onMouseEnter={() => {
+            swiperRef.current?.autoplay?.stop();
+            setSlidesPerView(4);
+        }}
+        onMouseLeave={() => {
+            swiperRef.current?.autoplay?.start();
+            setSlidesPerView(4);
+        }}>
             <Swiper
-                spaceBetween={14}
-                slidesPerView={6}
+                spaceBetween={0}
+                slidesPerView={slidesPerView}
                 loop={true}
-                navigation={true}
                 autoplay={{ delay: 2000, disableOnInteraction: false }}
-                modules={[Autoplay, Navigation]}
+                modules={[Autoplay]}
                 speed={1000}
                 className="flex items-center justify-between"
+                onSwiper={(swiper) => (swiperRef.current = swiper)}
             >
-                {[...Array(10)].map((_, index) => (
-                    <SwiperSlide key={index} className="flex justify-center">
-                        <div className="relative w-full h-full flex flex-col justify-center gap-[14px]">
-                            <img
-                                src={`/src/assets/images/newarrivals${(index % 4) + 1}.png`}
-                                className="object-cover h-[320px] w-[220px]"
-                                alt="Product"
-                            />
-                            <p className="text-black foont-medium text-[14px]">Athletics French Terry Crew</p>
-                            <p className="text-[18px] text-[#8F0024] font-semibold price">109.40 TND</p>
+                {[...Array(100)].map((_, index) => (
+                    <SwiperSlide key={index} className="flex justify-center bg-[#F3F3F3] border border-[#E6E6E6] transition-all duration-1000 hover:scale-[1.05] hover:h-[500px] hover:w-[375px] hover:z-10 relative">
+                        <div className="w-full h-full flex flex-col justify-center gap-[14px] bg-[#F3F3F3] p-[20px] transition-all duration-1000 hover:scale-[1.2] hover:h-[500px] hover:w-[375px] hover:z-10 relative">
+                            <div className="relative bg-white w-full h-[80%] flex items-center justify-center">
+                                <img src={`/src/assets/images/arrivals1.png`} className="" alt="Product" />
+                                <div className="absolute bottom-[0px] right-[0px] w-full h-[60px] border-t border-[#D6D6D5] flex items-center justify-between">
+                                    <div className="w-full flex items-center justify-center">Sweat Shirts</div>
+                                    <div className="h-[20.5px] bg-[#D6D6D5] w-[1px]"></div>
+                                    <div className="w-full flex items-center justify-center">840 Products</div>
+                                </div>
+                            </div>
                         </div>
                     </SwiperSlide>
                 ))}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import CategoriesSlider from "../components/home/CategoriesSlider";
 import Header from "../components/common/Header";
 import HeroSlider from "../components/home/HeroSlider";
 import CategoriesAndProducts from "../components/home/CategoriesAndProducts";
+import GridSlider from "../components/home/GridSlider";
 
 const Home = () => {
     return (
@@ -10,6 +11,16 @@ const Home = () => {
             <HeroSlider />
             <CategoriesSlider />
             <CategoriesAndProducts title="Recommended"/>
+            <div className="w-full flex flex-col items-center gap-[24px] mb-[40px]">
+                <div className="w-full flex items-center justify-between h-[48px] px-[50px]">
+                    <div className="w-full flex items-center gap-[32px]">
+                        <div className="text-[48px] font-normal leading-[32.78px] tracking-[0%]">New Arrivals</div>
+                    </div>
+                    <div className="h-[48px] bg-white border border-black px-[36px] py-[12px] cursor-pointer flex items-center justify-center font-semibold leading-[24px] tracking-[0%] whitespace-nowrap">VIEW ALL NEW</div>
+                </div>
+                <GridSlider />
+            </div>
+
             <CategoriesAndProducts title="Trending"/>
       </div>
   )
