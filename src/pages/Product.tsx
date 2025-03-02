4733fb169b1567139ff18c140ff4de1124a8bc74
SymplyMatt <symplymatt@gmail.com>
2025-03-02 16:10:02 +0100

    Add product details section with discount, reviews, and size selection; include new SVG icons

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -85,7 +85,72 @@ const Home = () => {
                 <div className="flex items-center justify-center">
                     <img src="/images/productimage1.png" className="h-full"/>
                 </div>
-                <div className="flex items-center justify-center"></div>
+                <div className="flex items-center justify-center p-[24px]">
+                    <div className="w-full flex flex-col items-center justify-center">
+                        <div className="h-[48px] bg-[#8F0024] w-full flex items-center justify-between text-white px-[24px] py-[12px]">
+                            <div className="font-normal text-[16px] leading-[24px] tracking-[0%]">SPECIAL DISCOUNT</div>
+                            <div className="font-semibold text-[16px] leading-[24px] tracking-[-4%]">18% OFF</div>
+                        </div>
+                        <div className="w-full flex flex-col justify-center items-center bg-white">
+                            <div className="w-full flex flex-col items-center py-[16px] px-[24px]">
+                                <div className="w-full flex items-center justify-between text-white">
+                                    <div className="text-[#141511] ">Arman classic varsity jacket</div>
+                                    <div className=""><img src="/images/heart.svg" className="cursor-pointer" /></div>
+                                </div>
+                                <div className="font-semibold text-[24px] leading-[24px] tracking-[-4%] w-full flex items-center gap-[8px] price">180.00 TND <span className="font-semibold text-[#8F0024] text-[20px] leading-[26px] tracking-[0%] line-through">220 TND</span></div>
+                            </div>
+                            <div className="w-full grid grid-cols-2 items-center border-t border-b border-[#D6D6D5]">
+                                <div className="px-[24px] col-span-1 flex flex-col justify-between text-white gap-[4px] border-r border-[#D6D6D5] py-[16px]">
+                                    <div className="text-[#676764] font-normal text-[16px] leading-[24px] tracking-[0%]">Category:</div>
+                                    <div className="text-[#141511] font-medium text-[18px] leading-[27px] tracking-[0%]">Men's Jacket</div>
+                                </div>
+                                <div className="px-[24px] col-span-1 flex flex-col justify-between text-white gap-[4px] py-[16px]">
+                                    <div className="text-[#141511] font-semibold text-[18px] leading-[27px] tracking-[0%]">4.5 <span className="font-normal">(103 Reviews)</span> </div>
+                                    <div className="flex items-center gap-[4px]">
+                                        <img src="/images/staractive.svg" />
+                                        <img src="/images/staractive.svg" />
+                                        <img src="/images/staractive.svg" />
+                                        <img src="/images/staractive.svg" />
+                                        <img src="/images/starinactive.svg" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="w-full flex items-center border-b border-[#D6D6D5]">
+                                <div className="px-[24px] w-full flex flex-col justify-between text-white gap-[4px] border-r border-[#D6D6D5] py-[16px]">
+                                    <div className="text-[#000000] font-semibold text-[18px] leading-[27px] tracking-[0%]">Color: <span className="font-normal">Green</span></div>
+                                    <div className="flex items-center gap-[9px]">
+                                        <div className="flex items-center justify-center h-[36px] w-[36px] rounded-50 border border-[#6B6B6B]">
+                                            <div className="h-[24px] w-[24px] rounded-50 bg-[#34301D] cursor-pointer"></div>
+                                        </div>
+                                        <div className="h-[24px] w-[24px] rounded-50 bg-[#1B376F] cursor-pointer"></div>
+                                        <div className="h-[24px] w-[24px] rounded-50 bg-[#401B6F] cursor-pointer"></div>
+                                        <div className="h-[24px] w-[24px] rounded-50 bg-[#6F371B] cursor-pointer"></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="w-full flex items-center border-b border-[#D6D6D5]">
+                                <div className="px-[24px] w-full flex flex-col justify-between text-white gap-[4px] border-r border-[#D6D6D5] py-[16px]">
+                                    <div className="flex w-full items-center justify-between">
+                                        <div className="text-[#141511]">Select size</div>
+                                        <div className="font-medium text-[14px] leading-[21px] tracking-[0%] text-[#676764] underline cursor-pointer">SIZE CHART</div>
+                                    </div>
+                                    <div className="w-full grid grid-cols-3 gap-[12px] text-[#141511]">
+                                        <div className="col-span-1 h-40 border border-[#D6D6D5] flex items-center justify-center cursor-pointer font-medium text-[16px] leading-[21px] tracking-[-4%] hover:border-[#141511]">XS</div>
+                                        <div className="col-span-1 h-40 border border-[#D6D6D5] flex items-center justify-center cursor-pointer font-medium text-[16px] leading-[21px] tracking-[-4%] hover:border-[#141511]">S</div>
+                                        <div className="col-span-1 h-40 border border-[#D6D6D5] flex items-center justify-center cursor-pointer font-medium text-[16px] leading-[21px] tracking-[-4%] hover:border-[#141511] bg-[#F3F3F3]">M</div>
+                                        <div className="col-span-1 h-40 border border-[#D6D6D5] flex items-center justify-center cursor-pointer font-medium text-[16px] leading-[21px] tracking-[-4%] hover:border-[#141511]">L</div>
+                                        <div className="col-span-1 h-40 border border-[#D6D6D5] flex items-center justify-center cursor-pointer font-medium text-[16px] leading-[21px] tracking-[-4%] hover:border-[#141511] bg-[#F3F3F3]">XL</div>
+                                        <div className="col-span-1 h-40 border border-[#D6D6D5] flex items-center justify-center cursor-pointer font-medium text-[16px] leading-[21px] tracking-[-4%] hover:border-[#141511]">XXL</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="w-full flex flex-col items-center justify-center px-[24px] py-[16px] gap-[12px]">
+                                <div className="gap-[8px] w-full h-[48px] bg-[#141511] text-white flex items-center justify-center cursor-pointer"><img src="/images/plus.svg"/> ADD TO CART</div>
+                                <div className="gap-[8px] w-full h-[48px] bg-[#fff] text-[#60D669] flex items-center justify-center cursor-pointer border border-[#60D669]"><img src="/images/plus.svg"/><img src="/images/whatsapp.svg"/> ORDER ON WHATSAPP</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <UpsellSlider/>
         </Layout>
