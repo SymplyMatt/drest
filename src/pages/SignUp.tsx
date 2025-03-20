c04eb6efcb8c0186af293fc7a058ef2fd215bc71
SymplyMatt <symplymatt@gmail.com>
2025-03-21 00:20:16 +0100

    Add Login and SignUp pages with routing and Redux integration

---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,36 @@
+import HeroSlider from "../components/home/HeroSlider";
+import CategoriesAndProducts from "../components/home/CategoriesAndProducts";
+import FeaturedBrands from "../components/home/FeaturedBrands";
+import NewArrivals from "../components/home/NewArrivals";
+import Layout from "./Layout";
+import FlashSales from "../components/home/FlashSales";
+import homeimagetwo from '../assets/images/homeimagetwo.png';
+import homeimagethree from '../assets/images/homeimagethree.png';
+import RoundCategoriesSlider from "../components/home/RoundCategoriesSlider";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../redux/store";
+import { setAuthPage } from "../redux/states/auth";
+const SignUp = () => {
+    const dispatch = useDispatch<AppDispatch>();
+    useEffect(()=>{
+        dispatch(setAuthPage("create-account"));
+    },[]);
+    return (
+        <Layout>
+            <HeroSlider />
+            <RoundCategoriesSlider />
+            <FlashSales />
+            <CategoriesAndProducts title="Recommended"/>
+            <NewArrivals />
+            <CategoriesAndProducts title="Trending"/>
+            <div className="w-full flex flex-col items-center justify-center">
+                <img src={homeimagetwo} className="w-full h-auto" />
+                <img src={homeimagethree} className="w-full h-auto" />
+            </div>
+            <FeaturedBrands />
+        </Layout>
+    );
+}
+
+export default SignUp;
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,12 +1,22 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import Product from "../pages/Product";
+import Login from "../pages/Login";
+import SignUp from "../pages/SignUp";
 
 const Routes = (): JSX.Element => {
   const pageRoutes = [
     {
       path: "/",
       element: <Home />
+    },
+    {
+      path: "/login",
+      element: <Login />
+    },
+    {
+      path: "/signup",
+      element: <SignUp />
     },
     {
       path: "/product/:id", 
