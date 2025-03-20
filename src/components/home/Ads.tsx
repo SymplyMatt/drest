fe14ee1dd32c6f5b093ce884d795da2c8aa67a75
SymplyMatt <symplymatt@gmail.com>
2025-03-21 00:27:51 +0100

    Add Ads component to Home page and remove inline images

---
+++ src/components/home/Ads.tsx
@@ -0,0 +1,12 @@
+import homeimagetwo from '../../assets/images/homeimagetwo.png';
+import homeimagethree from '../../assets/images/homeimagethree.png';
+const Ads = () => {
+  return (
+        <div className="w-full flex flex-col items-center justify-center">
+            <img src={homeimagetwo} className="w-full h-auto" />
+            <img src={homeimagethree} className="w-full h-auto" />
+        </div>
+  )
+}
+
+export default Ads
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,9 +4,8 @@ import FeaturedBrands from "../components/home/FeaturedBrands";
 import NewArrivals from "../components/home/NewArrivals";
 import Layout from "./Layout";
 import FlashSales from "../components/home/FlashSales";
-import homeimagetwo from '../assets/images/homeimagetwo.png';
-import homeimagethree from '../assets/images/homeimagethree.png';
 import RoundCategoriesSlider from "../components/home/RoundCategoriesSlider";
+import Ads from "../components/home/Ads";
 const Home = () => {
     return (
         <Layout>
@@ -16,10 +15,7 @@ const Home = () => {
             <CategoriesAndProducts title="Recommended"/>
             <NewArrivals />
             <CategoriesAndProducts title="Trending"/>
-            <div className="w-full flex flex-col items-center justify-center">
-                <img src={homeimagetwo} className="w-full h-auto" />
-                <img src={homeimagethree} className="w-full h-auto" />
-            </div>
+            <Ads />
             <FeaturedBrands />
         </Layout>
     );
