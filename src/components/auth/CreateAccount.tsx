629d1461055799cc387e6267fef693f4304f40b8
SymplyMatt <symplymatt@gmail.com>
2025-03-20 23:24:07 +0100

    Enhance CreateAccount component with improved layout and validation hints

--- src/components/auth/CreateAccount.tsx
+++ src/components/auth/CreateAccount.tsx
@@ -8,7 +8,7 @@ const CreateAccount = () => {
               <div className="flex flex-col items-center gap-[8px]">
                   <div className="text-center text-[28px] font-medium leading-[130%] tracking-[0%] text-[#141511]">Create your account</div>
               </div>
-              <div className="w-full flex flex-col justify-center gap-[8px]">
+              <div className="w-full flex flex-col justify-center gap-[12px]">
                   <label className="text-[#141511] font-semibold">Email</label>
                   <input type="text" className="bg-[#F3F3F3] outline-none border-none p-[8px] px-[12px] w-full h-[48px]" placeholder="mail@gmail.com"/>
                   <label className="text-[#141511] font-semibold">First name</label>
@@ -18,8 +18,17 @@ const CreateAccount = () => {
                     <input type="password" className="bg-[#F3F3F3] outline-none border-none p-[8px] px-[12px] w-full h-[48px]" placeholder=""/>
                     <img src="/images/eyepassword.svg" className="cursor-pointer absolute right-[10px]"/>
                   </div>
+                  <div className="flex flex-col justify-center gap-[8px]">
+                    <div className="w-full flex items-center justify-center gap-[8px]">
+                        <div className={`transition-all duration-300 h-[4px] flex-grow bg-[#D58618]`}> </div>
+                        <div className={`transition-all duration-300 h-[4px] flex-grow bg-[#D58618]`}> </div>
+                        <div className={`transition-all duration-300 h-[4px] flex-grow bg-[#D6D6D5]`}> </div>
+                    </div>
+                    <div className="text-[#676764] flex items-center gap-[8px] cursor-pointer text-left text-[14px]">At least 1 number, 8 characters, 1 symbol</div>
+                </div>
               </div>
-              <div className="flex h-[48px] bg-[#141511] w-full cursor-pointer text-white items-center justify-center">NEXT</div>
+              <div className="flex h-[48px] bg-[#141511] w-full cursor-pointer text-white items-center justify-center">REGISTER</div>
+              <div className="text-[#676764] flex items-center gap-[8px] cursor-pointer">Already have an account? <span className="font-semibold text-[#141511] underline">Sign in</span></div>
               <div className="text-[#676764]">Or</div>
               <div className="w-full flex flex-col justify-center gap-[12px]">
                   <div className="uppercase flex h-[36px] bg-[#fff] w-full cursor-pointer items-center justify-center border border-[#D6D6D5] text-[#141511] text-[14px] font-semibold gap-[8px]"><img src="/images/google.svg" /> Signup with Google</div>
