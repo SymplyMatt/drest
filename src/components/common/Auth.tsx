79e5a556751b74a7bdd92c2459241062562c9818
SymplyMatt <symplymatt@gmail.com>
2025-03-20 18:11:55 +0100

    Add OTP login component and integrate it into Auth component

--- src/components/common/Auth.tsx
+++ src/components/common/Auth.tsx
@@ -1,12 +1,14 @@
 import EmailLogin from "../auth/EmailLogin"
+import OTPLogin from "../auth/OTPLogin"
 import PasswordLogin from "../auth/PasswordLogin"
 
 const Auth = () => {
-    const page: string = 'passwordlogin'
+    const page: string = 'otp'
   return (
     <div className="w-[100vw] h-[100vh] fixed top-0 right-0 bg-[#1415114D] z-10 flex justify-center pt-[46px]">
         {page === 'emaillogin' && <EmailLogin />}
         {page === 'passwordlogin' && <PasswordLogin />}
+        {page === 'otp' && <OTPLogin />}
     </div>
   )
 }
