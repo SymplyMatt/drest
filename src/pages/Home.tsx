ac5ca413edbb6dacf46366c7e5b1914ec9de192b
SymplyMatt <symplymatt@gmail.com>
2025-03-20 15:43:41 +0100

    Add additional images to Home page layout for enhanced visual appeal

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,8 @@ import FeaturedBrands from "../components/home/FeaturedBrands";
 import NewArrivals from "../components/home/NewArrivals";
 import Layout from "./Layout";
 import FlashSales from "../components/home/FlashSales";
+import homeimagetwo from '../assets/images/homeimagetwo.png';
+import homeimagethree from '../assets/images/homeimagethree.png';
 const Home = () => {
     return (
         <Layout>
@@ -14,6 +16,10 @@ const Home = () => {
             <CategoriesAndProducts title="Recommended"/>
             <NewArrivals />
             <CategoriesAndProducts title="Trending"/>
+            <div className="w-full flex flex-col items-center justify-center">
+                <img src={homeimagetwo} className="w-full h-auto" />
+                <img src={homeimagethree} className="w-full h-auto" />
+            </div>
             <FeaturedBrands />
         </Layout>
     );
