dc19d50dfb6fc09bbf9b7736f845262da6b0ded0
SymplyMatt <symplymatt@gmail.com>
2025-03-21 02:55:37 +0100

    Add conditional rendering for heart icon in CategoriesAndProducts and include heartfilled SVG

--- src/components/home/CategoriesAndProducts.tsx
+++ src/components/home/CategoriesAndProducts.tsx
@@ -40,7 +40,8 @@ const CategoriesAndProducts: React.FC<CategoriesAndProductsProps> = ({ title='Tr
                         <div className="col-span-1 flex flex-col items-center h-[500px] group" key={number}>
                             <div className="w-full h-[400px] bg-[#F3F3F3] border border-[#E6E6E6] flex items-center justify-center relative">
                                 <img src={`/images/recommended${number+1}.png`} className="w-full h-full object-cover"/>
-                                <img src="/images/heart.svg" className="cursor-pointer absolute top-[20px] right-[10px] transition-transform duration-200 hover:scale-[0.9]" />
+                                {!location.pathname.includes('wishlist') && <img src="/images/heart.svg" className="cursor-pointer absolute top-[20px] right-[10px] transition-transform duration-200 hover:scale-[0.9]" />}
+                                {location.pathname.includes('wishlist') && <img src="/images/heartfilled.svg" className="cursor-pointer absolute top-[20px] right-[10px] transition-transform duration-200 hover:scale-[0.9]" />}
                                 <div className={`absolute top-[20px] left-[10px] h-[28px] bg-[#8F0024] p-[10px] flex justify-center items-center text-white text-[14px] font-semibold leading-[21px] tracking-[-4%] gap-[8px] ${!discount && 'hidden'}`}><img src="/images/discountbadge.svg" /> {discount}% OFF</div>
                                 <img src="/images/rec_plus.svg" className="cursor-pointer absolute bottom-[20px] right-[10px]" />
                                 <div className="w-full h-[80px] border-t border-b border-[#D6D6D5] cursor-pointer absolute bottom-[0px] right-[0px] bg-white flex items-center justify-center p-[16px] opacity-0 translate-y-4 transition-all duration-500 ease-in-out group-hover:opacity-100 group-hover:translate-y-0">
