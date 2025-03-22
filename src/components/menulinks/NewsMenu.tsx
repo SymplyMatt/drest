ed0f848ce3cf312b01e98e9b5c8373bf2645ff1a
SymplyMatt <symplymatt@gmail.com>
2025-03-22 01:30:07 +0100

    Update NewsMenu component to rename section and enhance item listings

--- src/components/menulinks/NewsMenu.tsx
+++ src/components/menulinks/NewsMenu.tsx
@@ -23,12 +23,13 @@ const NewsMenu: React.FC<MenuLinksProps> = ({ hoveredMenu, setHoveredMenu }) => 
     >
         <div className="py-[30px] pr-[70px] flex gap-[30px] flex-wrap border-r border-[#C4C4C4]">
             <div className="flex flex-col gap-[10px]">
-                <div className="text-[#141511] font-bold text-[16px] leading-[24px] tracking-[0%]">NEW & TRENDING</div>
+                <div className="text-[#141511] font-bold text-[16px] leading-[24px] tracking-[0%]">NEW & POPULAR</div>
                 <div className="flex flex-col gap-[10px] justify-center">
-                    <div className="text-[#4F4F4D] text-[16px] font-normal leading-[24px] tracking-[0%] cursor-pointer hover:text-[#8F0024] hover:font-semibold transition-colors duration-200">Featured items</div>
-                    <div className="text-[#4F4F4D] text-[16px] font-normal leading-[24px] tracking-[0%] cursor-pointer hover:text-[#8F0024] hover:font-semibold transition-colors duration-200">New Arrivals</div>
-                    <div className="text-[#4F4F4D] text-[16px] font-normal leading-[24px] tracking-[0%] cursor-pointer hover:text-[#8F0024] hover:font-semibold transition-colors duration-200">Trending</div>
-                    <div className="text-[#4F4F4D] text-[16px] font-normal leading-[24px] tracking-[0%] cursor-pointer hover:text-[#8F0024] hover:font-semibold transition-colors duration-200">Recommended</div>
+                    <div className="text-[#4F4F4D] text-[16px] font-normal leading-[24px] tracking-[0%] cursor-pointer hover:text-[#8F0024] hover:font-semibold transition-colors duration-200">New arrivals</div>
+                    <div className="text-[#4F4F4D] text-[16px] font-normal leading-[24px] tracking-[0%] cursor-pointer hover:text-[#8F0024] hover:font-semibold transition-colors duration-200">Trending/Popular</div>
+                    <div className="text-[#4F4F4D] text-[16px] font-normal leading-[24px] tracking-[0%] cursor-pointer hover:text-[#8F0024] hover:font-semibold transition-colors duration-200">New Dresses</div>
+                    <div className="text-[#4F4F4D] text-[16px] font-normal leading-[24px] tracking-[0%] cursor-pointer hover:text-[#8F0024] hover:font-semibold transition-colors duration-200">New Bags</div>
+                    <div className="text-[#4F4F4D] text-[16px] font-normal leading-[24px] tracking-[0%] cursor-pointer hover:text-[#8F0024] hover:font-semibold transition-colors duration-200">New Accessories</div>
                 </div>
             </div>
         </div>
