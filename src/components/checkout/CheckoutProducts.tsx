57e90a9bb3b32ca506b5c7d4ad659e49b71c4c29
SymplyMatt <symplymatt@gmail.com>
2025-03-23 18:18:31 +0100

    Refactor CheckoutProducts component to enhance order review layout and improve product detail presentation

--- src/components/checkout/CheckoutProducts.tsx
+++ src/components/checkout/CheckoutProducts.tsx
@@ -22,33 +22,37 @@ const CheckoutProducts = () => {
             </div>
         </div>
         <div className="w-full flex flex-col p-[24px] gap-[24px] border-b border-r border-[#D6D6D5]">
-            {cart.map((_, index)=>(
-                <div className="flex items-center justify-between gap-[24px]" key={index}>
-                    <div className="flex items-center gap-[12px] h-[96px]">
-                        <img src={`/images/cartimage${index + 1}.svg`} className="h-full"/>
-                        <div className="flex flex-col justify-between h-full py-[8px]">
-                            <div className="text-[#141511] text-[14px] font-semibold whitespace-nowrap">Ocean breeze varsity jacket</div>
-                            <div className="flex flex-col">
-                                <div className="text-[#4F4F4D]">Category: Verset jacket</div>
-                                <div className="text-[#4F4F4D]">Color: Solid Blue</div>
+            <div className="text-[#141511] font-bold gap-[24px]">Please review your order</div>
+            <div className="w-full flex flex-col gap-[12px]">
+                <div className="text-[#676764]">Product details</div>
+                {cart.map((_, index)=>(
+                    <div className="flex items-center justify-between gap-[24px]" key={index}>
+                        <div className="flex items-center gap-[12px] h-[96px]">
+                            <img src={`/images/cartimage${index + 1}.svg`} className="h-full"/>
+                            <div className="flex flex-col justify-between h-full py-[8px]">
+                                <div className="text-[#141511] text-[14px] font-semibold whitespace-nowrap">Ocean breeze varsity jacket</div>
+                                <div className="flex flex-col">
+                                    <div className="text-[#4F4F4D]">Category: Verset jacket</div>
+                                    <div className="text-[#4F4F4D]">Color: Solid Blue</div>
+                                </div>
                             </div>
                         </div>
+                        <div className="flex flex-col gap-[8px] justify-center">
+                            <div className="text-[#676764]">Size</div>
+                            <div className="text-[#141511] text-[18px]">XL</div>
+                        </div>
+                        <div className="flex flex-col gap-[8px] justify-center">
+                            <div className="text-[#676764]">Quantity</div>
+                            <div className="text-[#141511] text-[18px]">1 Pcs</div>
+                        </div>
+                        <div className="flex flex-col gap-[8px] justify-center">
+                            <div className="text-[#676764]">Total</div>
+                            <div className="text-[#141511] text-[18px]">120.00 TND</div>
+                        </div>
+                        <div className="border border-[#D6D6D5] cursor-pointer text-[#141511] font-semibold h-[40px] px-[24px] flex items-center justify-center">EDIT</div>
                     </div>
-                    <div className="flex flex-col gap-[8px] justify-center">
-                        <div className="text-[#676764]">Size</div>
-                        <div className="text-[#141511] text-[18px]">XL</div>
-                    </div>
-                    <div className="flex flex-col gap-[8px] justify-center">
-                        <div className="text-[#676764]">Quantity</div>
-                        <div className="text-[#141511] text-[18px]">1 Pcs</div>
-                    </div>
-                    <div className="flex flex-col gap-[8px] justify-center">
-                        <div className="text-[#676764]">Total</div>
-                        <div className="text-[#141511] text-[18px]">120.00 TND</div>
-                    </div>
-                    <div className="border border-[#D6D6D5] cursor-pointer text-[#141511] font-semibold h-[40px] px-[24px] flex items-center justify-center">EDIT</div>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     </div>
   )
