6484202ca0d2132fa69458329a6bb463c5687f5c
SymplyMatt <symplymatt@gmail.com>
2025-04-13 02:33:06 +0100

    product list in checkout

---
+++ src/components/checkout/ProductList.tsx
@@ -0,0 +1,85 @@
+import { Swiper, SwiperClass, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { useRef, useState } from "react";
+import { Autoplay } from "swiper/modules";
+import { useNavigate } from "react-router-dom";
+const ProductList = () => {
+    const swiperRef = useRef<SwiperClass | null>(null);
+    const [activeIndex, setActiveIndex] = useState(0);
+    const navigate = useNavigate();
+    const renderCustomPagination = () => {
+        const totalSlides = 6; 
+        return (
+            <div className="h-[660px] flex flex-col items-center justify-center mb-6 gap-[24px]">
+                <div className="h-full flex flex-col items-center justify-center">
+                    {[...Array(totalSlides)].map((_, index) => (
+                        <button
+                            key={index}
+                            onClick={() => swiperRef.current?.slideTo(index)}
+                            className={`transition-all duration-300 ${
+                                activeIndex === index 
+                                    ? "bg-[#141511] w-[4px] h-full" 
+                                    : "bg-[#F3F3F3] w-[4px] h-full"
+                            }`}
+                            aria-label={`Go to slide ${index + 1}`}
+                        />
+                    ))}
+                </div>
+            </div>
+        );
+    };
+  return (
+    <div className="w-full flex flex-col gap-4 product-list-swiper p-[24px]">
+        <div className="w-full flex items-center justify-between">
+            <div className="text-[#141511] text-[20px] font-medium">Product list</div>
+            <div className="cursor-pointer text-[#676764] text-[14px] underline leading-[150%]">EDIT PRODUCTS</div>
+        </div>
+        <div className="w-full flex flex-col items-center justify-between">
+            <div className="w-full h-[660px] flex justify-between w-full overflow-hidden">
+                <Swiper
+                    direction="vertical"
+                    slidesPerView={5.2}
+                    spaceBetween={20}
+                    loop={true} 
+                    autoplay={{ delay: 3000, disableOnInteraction: false }}
+                    speed={1000}
+                    className="w-full h-full overflow-hidden"
+                    onSwiper={(swiper) => (swiperRef.current = swiper)}
+                    onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
+                    modules={[Autoplay]}
+                >
+                    {[1,2,3,4,5,6].map((item, index) => (
+                        <SwiperSlide className="w-full h-[111px]" key={index}>
+                            <div className="w-full flex items-center gap-3 h-full">
+                                <img src={`/images/payment-image${item}.svg`} alt={`Product ${item}`} className="h-full object-cover" />
+                                <div className="w-full flex flex-col h-full justify-between py-1">
+                                    <div className="text-[#141511] text-lg font-medium leading-[150%]">Ocean breeze varsity jacket</div>
+                                    <div className="text-[#141511] text-xl font-medium price">120.00 TND</div>
+                                    <div className="w-full grid grid-cols-10 items-center justify-between gap-2">
+                                        <div className="col-span-6 text-[#4F4F4D] text-base leading-[150%]">Color: <span>Solid Blue</span></div>
+                                        <div className="col-span-4 text-[#4F4F4D] text-base leading-[150%]">Size: <span>XL</span></div>
+                                    </div>
+                                    <div className="w-full grid grid-cols-10 items-center justify-between gap-2">
+                                        <div className="col-span-6 text-[#4F4F4D] text-base leading-[150%]">Category: <span>{("varsity jacket").substring(0,13)}{("varsity jacket").length > 13 ? '...' : ''}</span></div>
+                                        <div className="col-span-4 text-[#4F4F4D] text-base leading-[150%]">Quantity: <span>1 Pcs</span></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+                <div className="h-full">{renderCustomPagination()}</div>
+            </div>
+            <div className="flex w-full items-center justify-center bg-[#141511] cursor-pointer h-[48px] text-white mt-[12px]" 
+                    onClick={()=>{
+                        navigate(`/checkout/`);
+                    }}>
+                    CONTINUE
+                </div>
+        </div>
+    </div>
+  )
+}
+
+export default ProductList
--- src/components/checkout/Summary.tsx
+++ src/components/checkout/Summary.tsx
@@ -1,47 +1,63 @@
 import { useLocation, useNavigate } from "react-router-dom";
+import ProductList from "./ProductList";
 
 const Summary = () => {
     const navigate = useNavigate();
     const page = useLocation().pathname.split("/").pop();
     return (
-        <div className="w-full col-span-1 flex flex-col border-b border-[#D6D6D5]">
-            <div className="w-full flex items-center justify-center border-b border-r border-[#D6D6D5] px-[50px] py-[12px]">
-                <div className="text-[#141511] text-[20px] font-medium">ORDER SUMMARY</div>
-            </div>
-            <div className="w-full p-[24px] flex flex-col items-center gap-[12px]">
-                <div className="w-full flex items-center justify-between">
-                    <div className="text-[#4F4F4D] text-[18px] flex items-center gap-[4px]">Subtotal (6 items) <img src="/images/question.svg" className="cursor-pointer" /></div>
-                    <div className="text-[#4F4F4D] text-[20px] flex items-center gap-[8px]">754.50 TND</div>
+        <div className="w-full col-span-1 flex flex-col border-b border-[#D6D6D5] justify-between">
+            <div className="w-full flex flex-col">
+                <div className="w-full flex items-center justify-center border-b border-r border-[#D6D6D5] px-[50px] py-[12px]">
+                    <div className="text-[#141511] text-[20px] font-medium">ORDER SUMMARY</div>
                 </div>
-                <div className="w-full flex items-center justify-between">
-                    <div className="text-[#4F4F4D] text-[20px] flex items-center gap-[8px]">Discount <span className="text-[#8F0024] font-medium">10% OFF</span></div>
-                    <div className="text-[#4F4F4D] text-[20px] flex items-center gap-[8px]">-75.45 TND</div>
-                </div>
-                <div className="w-full flex items-center justify-between">
-                    <div className="text-[#141511] text-[18px] flex items-center gap-[4px] font-semibold">Total payment </div>
-                    <div className="text-[#4F4F4D] text-[20px] flex items-center gap-[8px]">697.05 TND</div>
-                </div>
-                <div className="w-full h-[1px] bg-[#D6D6D5] mt-[12px]"></div>
-                {page !== 'payment' ? <>
-                    <div className="w-full flex items-center justify-between mt-[12px]">
-                        <div className="text-[#141511] text-[18px] flex items-center gap-[4px] font-medium">Do you have a promo code? </div>
-                        <div className="text-[#4F4F4D] text-[20px] flex items-center gap-[8px]"><img src="/images/caretup.svg" className="cursor-pointer" /></div>
+                <div className="w-full p-[24px] flex flex-col items-center gap-[12px]">
+                    <div className="w-full flex items-center justify-between">
+                        <div className="text-[#4F4F4D] text-[18px] flex items-center gap-[4px]">Total Products</div>
+                        <div className="text-[#4F4F4D] text-[20px] flex items-center gap-[8px]">6 Products</div>
                     </div>
-                    <div className="w-full h-[48px] bg-[#F3F3F3] flex items-center justify-between py-[4px] pl-[12px] pr-[4px]">
-                        <div className="text-[#141511] ">ONEDISCOUNT</div>
-                        <div className="h-[40px] flex items-center bg-white border border-[#D6D6D5] py-[8px] px-[24px] text-[#676764]">APPLIED</div>
+                    <div className="w-full flex items-center justify-between">
+                        <div className="text-[#4F4F4D] text-[18px] flex items-center gap-[4px]">Subtotal<img src="/images/question.svg" className="cursor-pointer" /></div>
+                        <div className="text-[#4F4F4D] text-[20px] flex items-center gap-[8px]">754.50 TND</div>
                     </div>
-                    <div className="flex gap-[8px] items-start text-[#676764]"><img src="/images/info_sm.svg" /> To get a promo code you can help our social media, there we share promo codes every month.</div>
+                    <div className="w-full flex items-center justify-between">
+                        <div className="text-[#4F4F4D] text-[20px] flex items-center gap-[8px]">Estimated shipping</div>
+                        <div className="text-[#4F4F4D] text-[20px] flex items-center gap-[8px]">0.00 TND</div>
+                    </div>
+                    <div className="w-full flex items-center justify-between">
+                        <div className="text-[#4F4F4D] text-[20px] flex items-center gap-[8px]">Estimated tax</div>
+                        <div className="text-[#4F4F4D] text-[20px] flex items-center gap-[8px]">20.00 TND</div>
+                    </div>
+                    <div className="w-full flex items-center justify-between">
+                        <div className="text-[#4F4F4D] text-[20px] flex items-center gap-[8px]">Discount <span className="text-[#E0523F] font-medium">10% OFF</span></div>
+                        <div className="text-[#4F4F4D] text-[20px] flex items-center gap-[8px]">-75.45 TND</div>
+                    </div>
+                    <div className="w-full flex items-center justify-between">
+                        <div className="text-[#141511] text-[18px] flex items-center gap-[4px] font-semibold">Total payment </div>
+                        <div className="text-[#4F4F4D] text-[20px] flex items-center gap-[8px] font-semibold">697.05 TND</div>
+                    </div>
                     <div className="w-full h-[1px] bg-[#D6D6D5] mt-[12px]"></div>
-                    <div className="flex w-full items-center justify-center bg-[#141511] cursor-pointer h-[48px] text-white mt-[12px]" 
-                        onClick={()=>{
-                            const nextPage = page === 'cart' ? 'delivery' : page === 'delivery' ? 'confirm' : page === 'confirm' ? 'payment' : 'payment';
-                            navigate(`/checkout/${nextPage}`);
-                        }}>
-                        CHECKOUT
-                    </div>
-                </> : ''}
+                    {page !== 'payment' ? <>
+                        <div className="w-full flex items-center justify-between mt-[12px]">
+                            <div className="text-[#141511] text-[18px] flex items-center gap-[4px] font-medium">Do you have a promo code? </div>
+                            <div className="text-[#4F4F4D] text-[20px] flex items-center gap-[8px]"><img src="/images/caretup.svg" className="cursor-pointer" /></div>
+                        </div>
+                        <div className="w-full h-[48px] bg-[#F3F3F3] flex items-center justify-between py-[4px] pl-[12px] pr-[4px]">
+                            <div className="text-[#141511] ">ONEDISCOUNT</div>
+                            <div className="h-[40px] flex items-center bg-white border border-[#D6D6D5] py-[8px] px-[24px] text-[#676764]">APPLIED</div>
+                        </div>
+                        <div className="flex gap-[8px] items-start text-[#676764]"><img src="/images/info_sm.svg" /> To get a promo code you can help our social media, there we share promo codes every month.</div>
+                        <div className="w-full h-[1px] bg-[#D6D6D5] mt-[12px]"></div>
+                        <div className="flex w-full items-center justify-center bg-[#141511] cursor-pointer h-[48px] text-white mt-[12px]" 
+                            onClick={()=>{
+                                const nextPage = page === 'cart' ? 'delivery' : page === 'delivery' ? 'confirm' : page === 'confirm' ? 'payment' : 'payment';
+                                navigate(`/checkout/${nextPage}`);
+                            }}>
+                            CHECKOUT
+                        </div>
+                    </> : ''}
+                </div>
             </div>
+            {page === 'payment' ? <ProductList /> : ''}
         </div>
     )
 }
