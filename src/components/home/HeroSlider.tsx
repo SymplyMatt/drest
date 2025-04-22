34b194f816e7650b115542a6d3319a08a8f738cd
SymplyMatt <symplymatt@gmail.com>
2025-04-22 15:05:25 +0100

    Update HeroSlider component to use dynamic content for titles, descriptions, and buttons based on selected language

--- src/components/home/HeroSlider.tsx
+++ src/components/home/HeroSlider.tsx
@@ -27,11 +27,11 @@ const HeroSlider = () => {
                                 { heroSlider[language][0].title1 } <br /> { heroSlider[language][0].title2 }
                             </div>
                             <div className="text-[14px] max-w-[370px] text-[#E6E6E6]">
-                                Celebrate love in style! Shop our exclusive Valentine's Day collection and enjoy up to 50% off on romantic dresses, chic accessories, and more.
+                                { heroSlider[language][0].description }
                             </div>
                         </div>
                         <div className="bg-white h-[48px] w-fit px-[36px] text-black flex items-center justify-center tracking-[2%] text-[16px] font-medium cursor-pointer transition-transform duration-200 hover:scale-90 transition-transform duration-200 hover:scale-[0.9]">
-                            SHOP NOW
+                            { heroSlider[language][0].btn }
                         </div>
                     </div>
                 </SwiperSlide>
@@ -39,14 +39,14 @@ const HeroSlider = () => {
                     <div className="w-full flex justify-center bg-no-repeat bg-cover px-[80px] gap-[32px] flex-col text-white slidertwo">
                         <div className="flex flex-col justify-center gap-[12px]">
                             <div className="text-white text-[40px] leading-[50px] header_slider">
-                                Fresh Styles, Just for You!
+                            { heroSlider[language][1].title1 }
                             </div>
                             <div className="text-[14px] max-w-[370px] text-[#E6E6E6]">
-                                Our new arrivals are here to elevate your wardrobe. Discover the latest trends and be the first to own the season's hottest looks!
+                                { heroSlider[language][1].description }
                             </div>
                         </div>
                         <div className="bg-black text-[#E6E6E6] h-[48px] w-fit px-[36px] flex items-center justify-center tracking-[2%] text-[16px] font-medium cursor-pointer transition-transform duration-200 hover:scale-90">
-                            SHOP NOW
+                            { heroSlider[language][1].btn }
                         </div>
                     </div>
                 </SwiperSlide>
