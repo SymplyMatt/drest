81b9116102093cebdcccb225a06d9aa2cdaa9bb8
SymplyMatt <symplymatt@gmail.com>
2025-05-03 17:05:35 +0100

    Refactor MobileFooter component to enhance readability and update path handling

--- src/components/common/MobileFooter.tsx
+++ src/components/common/MobileFooter.tsx
@@ -3,20 +3,23 @@ import { AppDispatch, RootState } from "../../redux/store";
 import { setAuthPage } from "../../redux/states/auth";
 import { useSelector } from "react-redux";
 import { setSearchMode } from "../../redux/states/app";
+import { useLocation } from "react-router-dom";
 
 const MobileFooter = () => {
     const dispatch = useDispatch<AppDispatch>();
-  const { authPage } = useSelector((state: RootState) => state.auth);
-  const { loggedInUser, searchMode } = useSelector((state: RootState) => state.app);
+    const { authPage } = useSelector((state: RootState) => state.auth);
+    const { loggedInUser, searchMode } = useSelector((state: RootState) => state.app);
+    const location = useLocation();
+    const currentPath = location.pathname.split('/')[1];
     return (
         <div className="fixed bottom-0 left-0 w-full h-[80px] bg-[#FFFFFF] z-10 bg-black px-[26px] tmd:hidden grid grid-cols-5 items-center justify-between border border-[#EAEAEA] gap-[12px]">
-            <div className="col-span-1 flex flex-col items-center gap-[4px] text-center text-[#912A1D] text-[12px] font-bold"
+            <div className={`col-span-1 flex flex-col items-center gap-[4px] text-center text-[12px] ${(!authPage  && currentPath == '') ? 'font-bold' : 'font-medium'} ${(!authPage  && currentPath == '') ? 'text-[#912A1D]' : 'text-[#141511]'}`}
                 onClick={() => {
                     authPage && dispatch(setAuthPage(null));
                     searchMode && dispatch(setSearchMode(null));
                 }}>
                 <svg width="17" height="18" viewBox="0 0 17 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M0.5 18V6L8.5 0L16.5 6V18H10.5V11H6.5V18H0.5Z" fill="#912A1D"/>
+                    <path d="M0.5 18V6L8.5 0L16.5 6V18H10.5V11H6.5V18H0.5Z" fill={(!authPage  && currentPath == '') ? "#912A1D" : 'black'}/>
                 </svg>
                 Home
             </div>
@@ -38,13 +41,13 @@ const MobileFooter = () => {
                 </svg>
                 Wishlist
             </div>
-            <div className="col-span-1 flex flex-col items-center gap-[4px] text-center text-[#141511] text-[12px] font-medium" 
+            <div className={`col-span-1 flex flex-col items-center gap-[4px] text-center text-[12px] ${(authPage) ? 'font-bold' : 'font-medium'} ${authPage ? 'text-[#912A1D]' : ''}`} 
                 onClick={() => {
                     !loggedInUser && !authPage && dispatch(setAuthPage("emaillogin"))
                 }}>
                 <svg width="23" height="22" viewBox="0 0 23 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" clip-rule="evenodd" d="M15.5 8C15.5 9.06087 15.0786 10.0783 14.3284 10.8284C13.5783 11.5786 12.5609 12 11.5 12C10.4391 12 9.42172 11.5786 8.67157 10.8284C7.92143 10.0783 7.5 9.06087 7.5 8C7.5 6.93913 7.92143 5.92172 8.67157 5.17157C9.42172 4.42143 10.4391 4 11.5 4C12.5609 4 13.5783 4.42143 14.3284 5.17157C15.0786 5.92172 15.5 6.93913 15.5 8ZM13.5 8C13.5 8.53043 13.2893 9.03914 12.9142 9.41421C12.5391 9.78929 12.0304 10 11.5 10C10.9696 10 10.4609 9.78929 10.0858 9.41421C9.71071 9.03914 9.5 8.53043 9.5 8C9.5 7.46957 9.71071 6.96086 10.0858 6.58579C10.4609 6.21071 10.9696 6 11.5 6C12.0304 6 12.5391 6.21071 12.9142 6.58579C13.2893 6.96086 13.5 7.46957 13.5 8Z" fill="black"/>
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M11.5 0C5.425 0 0.5 4.925 0.5 11C0.5 17.075 5.425 22 11.5 22C17.575 22 22.5 17.075 22.5 11C22.5 4.925 17.575 0 11.5 0ZM2.5 11C2.5 13.09 3.213 15.014 4.408 16.542C5.24744 15.4401 6.33015 14.5471 7.57164 13.9327C8.81312 13.3183 10.1798 12.9991 11.565 13C12.9324 12.9984 14.2821 13.3091 15.5111 13.9084C16.7402 14.5077 17.8162 15.3797 18.657 16.458C19.5234 15.3216 20.1068 13.9952 20.3589 12.5886C20.611 11.182 20.5244 9.73553 20.1065 8.36898C19.6886 7.00243 18.9512 5.75505 17.9555 4.73004C16.9598 3.70503 15.7343 2.93186 14.3804 2.47451C13.0265 2.01716 11.5832 1.88877 10.1699 2.09997C8.75652 2.31117 7.41379 2.85589 6.25277 3.68905C5.09175 4.52222 4.14581 5.61987 3.49323 6.8912C2.84065 8.16252 2.50018 9.57097 2.5 11ZM11.5 20C9.43391 20.0033 7.43014 19.2926 5.828 17.988C6.47281 17.0646 7.33119 16.3107 8.33008 15.7905C9.32896 15.2702 10.4388 14.999 11.565 15C12.6772 14.999 13.7735 15.2635 14.763 15.7713C15.7524 16.2792 16.6064 17.0158 17.254 17.92C15.6395 19.267 13.6026 20.0033 11.5 20Z" fill="black"/>
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M15.5 8C15.5 9.06087 15.0786 10.0783 14.3284 10.8284C13.5783 11.5786 12.5609 12 11.5 12C10.4391 12 9.42172 11.5786 8.67157 10.8284C7.92143 10.0783 7.5 9.06087 7.5 8C7.5 6.93913 7.92143 5.92172 8.67157 5.17157C9.42172 4.42143 10.4391 4 11.5 4C12.5609 4 13.5783 4.42143 14.3284 5.17157C15.0786 5.92172 15.5 6.93913 15.5 8ZM13.5 8C13.5 8.53043 13.2893 9.03914 12.9142 9.41421C12.5391 9.78929 12.0304 10 11.5 10C10.9696 10 10.4609 9.78929 10.0858 9.41421C9.71071 9.03914 9.5 8.53043 9.5 8C9.5 7.46957 9.71071 6.96086 10.0858 6.58579C10.4609 6.21071 10.9696 6 11.5 6C12.0304 6 12.5391 6.21071 12.9142 6.58579C13.2893 6.96086 13.5 7.46957 13.5 8Z" fill={(authPage) ? "#912A1D" : 'black'}/>
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M11.5 0C5.425 0 0.5 4.925 0.5 11C0.5 17.075 5.425 22 11.5 22C17.575 22 22.5 17.075 22.5 11C22.5 4.925 17.575 0 11.5 0ZM2.5 11C2.5 13.09 3.213 15.014 4.408 16.542C5.24744 15.4401 6.33015 14.5471 7.57164 13.9327C8.81312 13.3183 10.1798 12.9991 11.565 13C12.9324 12.9984 14.2821 13.3091 15.5111 13.9084C16.7402 14.5077 17.8162 15.3797 18.657 16.458C19.5234 15.3216 20.1068 13.9952 20.3589 12.5886C20.611 11.182 20.5244 9.73553 20.1065 8.36898C19.6886 7.00243 18.9512 5.75505 17.9555 4.73004C16.9598 3.70503 15.7343 2.93186 14.3804 2.47451C13.0265 2.01716 11.5832 1.88877 10.1699 2.09997C8.75652 2.31117 7.41379 2.85589 6.25277 3.68905C5.09175 4.52222 4.14581 5.61987 3.49323 6.8912C2.84065 8.16252 2.50018 9.57097 2.5 11ZM11.5 20C9.43391 20.0033 7.43014 19.2926 5.828 17.988C6.47281 17.0646 7.33119 16.3107 8.33008 15.7905C9.32896 15.2702 10.4388 14.999 11.565 15C12.6772 14.999 13.7735 15.2635 14.763 15.7713C15.7524 16.2792 16.6064 17.0158 17.254 17.92C15.6395 19.267 13.6026 20.0033 11.5 20Z" fill={(authPage) ? "#912A1D" : 'black'} />
                 </svg>
                 Account
             </div>
