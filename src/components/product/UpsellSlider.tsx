043f43d196bdadfaae633d7b1f2f9ccb10e0e21e
SymplyMatt <symplymatt@gmail.com>
2025-05-24 17:15:28 +0100

    Refactor UpsellSlider to use full product list and create UpsellSliderProduct component for better modularity

--- src/components/product/UpsellSlider.tsx
+++ src/components/product/UpsellSlider.tsx
@@ -1,35 +1,21 @@
-import { useNavigate } from "react-router-dom";
 import { Swiper, SwiperClass, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { Autoplay } from "swiper/modules";
-import {ReactNode, useEffect, useRef, useState } from "react";
+import {ReactNode, useRef, useState } from "react";
+import { Product } from "../../utils/utils";
+import UpsellSliderProduct from "./UpsellSliderProduct";
 interface CategoriesAndProductsProps {
-    products?: any[];
+    products: Product[];
     showTitle?: boolean;
     title?: string;
     titleComponent?: ReactNode;
 }
 
-const UpsellSlider: React.FC<CategoriesAndProductsProps> = ({ products = [1, 2, 3, 4, 5, 6, 7, 8], showTitle = true, title= 'You may also like', titleComponent=<></> }) => {
-    const navigate = useNavigate();
-    const allImages = ['/images/recommended1.png','/images/recommended2.png','/images/recommended3.png','/images/recommended4.png','/images/recommended5.png','/images/recommended6.png','/images/recommended7.png','/images/recommended8.png']
+const UpsellSlider: React.FC<CategoriesAndProductsProps> = ({ products, showTitle = true, title= 'You may also like', titleComponent=<></> }) => {
     const swiperRef = useRef<SwiperClass | null>(null);
-    const [hoveredIndex, setHoveredIndex] = useState<number | null>(0);
-    const [activeImage, setActiveImage] = useState<string>('/images/recommended1.png');
     const [activeIndex, setActiveIndex] = useState(0);
-    useEffect(() => {
-        if (hoveredIndex) {
-            const intervalId = setInterval(() => {
-                const nextIndex:number = (allImages.indexOf(activeImage as string) < allImages.length - 1) ? allImages.indexOf(activeImage) + 1 : 0;
-                setActiveImage(allImages[nextIndex]);
-            }, 1000);
-            return () => clearInterval(intervalId);
-        } else {
-            setActiveImage('/images/recommended1.png');
-        }
-    }, [hoveredIndex, allImages]);    
     const handlePrevClick = () => {
         if (swiperRef.current) {
             swiperRef.current.slidePrev();
@@ -42,7 +28,7 @@ const UpsellSlider: React.FC<CategoriesAndProductsProps> = ({ products = [1, 2, 
     };
 
     const renderCustomPagination = () => {
-        const totalSlides = 5; 
+        const totalSlides = products.length; 
         return (
             <div className="w-full tmd:w-fit flex items-center justify-center gap-[24px] tmd:mt-[24px] p-[20px] tmd:p-[0px] mx-auto">
                 <img src="/images/prev_arrivals.svg" className="cursor-pointer" onClick={handlePrevClick} />
@@ -103,54 +89,11 @@ const UpsellSlider: React.FC<CategoriesAndProductsProps> = ({ products = [1, 2, 
                         },
                     }}
                 >
-                    {products.map((number,index) => {
-                        const discount = number % 3 !== 0 ? 0 : Math.floor(number/100 * 100) + 10;
-                        const price = Math.floor(number/100 * 101) + 100;
-                        const priceAfterDiscount = price - (price * discount) / 100;
-                        return (
-                            <SwiperSlide key={index}>
-                                <div className="flex flex-col items-center w-[300px] h-[400px] tmd:h-[480px] tmd:w-fit group cursor-pointer" 
-                                    onMouseEnter={() =>{ 
-                                        setHoveredIndex(index);
-                                    }} 
-                                    onMouseLeave={() => {
-                                        setHoveredIndex(null);
-                                    }}>
-                                    <div className="w-full w-[300px] tmd:w-fit h-[400px] tmd:h-[480px] tmd:w-fit bg-[#F3F3F3] border border-[#E6E6E6] flex items-center justify-center relative">
-                                        <img src={(hoveredIndex && hoveredIndex === index) ? activeImage : `/images/recommended${number}.png`} className="w-full h-full object-cover" />
-                                        <img src="/images/heart.svg" className="cursor-pointer absolute top-[20px] right-[10px] transition-transform duration-200 hover:scale-[0.9]" />
-                                        <div
-                                            className={`absolute top-[20px] left-[10px] h-[28px] bg-[#8F0024] p-[10px] flex justify-center items-center text-white text-[14px] font-semibold leading-[21px] tracking-[-4%] gap-[8px] ${
-                                                !discount && "hidden"
-                                            }`}
-                                        >
-                                            <img src="/images/discountbadge.svg" /> {discount}% OFF
-                                        </div>
-                                        <img src="/images/rec_plus.svg" className="cursor-pointer absolute bottom-[20px] right-[10px]" />
-                                        <div className="w-full h-[80px] border-t border-b border-[#D6D6D5] cursor-pointer absolute bottom-[0px] right-[0px] bg-white flex items-center justify-center p-[16px] opacity-0 translate-y-4 transition-all duration-500 ease-in-out group-hover:opacity-100 group-hover:translate-y-0">
-                                            <div
-                                                className="h-[48px] bg-[#141511] w-full flex items-center justify-center text-white gap-[8px] text-[16px] leading-[24px] tracking-[0%] font-medium transition-transform duration-200 hover:scale-[0.9]"
-                                                onClick={() => navigate(`/product/${number}`)}
-                                            >
-                                                <img src="/images/eye_product.svg" /> Quick View
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="flex flex-col items-center justify-center gap-[4px] h-[80px] border-b border-l border-r border-[#E6E6E6] w-full">
-                                        <div className="text-[18px] leading-[27px] tracking-[0%]">Ocean breeze varsity jacket</div>
-                                        <div className="text-[18px] font-semibold leading-[26px] tracking-[-5%] price">
-                                            {Math.round(priceAfterDiscount)} TND
-                                            {discount ? (
-                                                <span className="font-semibold text-[#8F0024] text-[16px] leading-[24px] tracking-[-4%] line-through">{price} TND</span>
-                                            ) : (
-                                                ""
-                                            )}
-                                        </div>
-                                    </div>
-                                </div>
-                            </SwiperSlide>
-                        );
-                    })}
+                    {products.map((product, index) => (
+                        <SwiperSlide key={index}>
+                            <UpsellSliderProduct product={product} />
+                        </SwiperSlide>
+                    ))}
                 </Swiper>
             </div>
             {renderCustomPagination()}
