ab9b677fcb2e2b0f39105cb2cab18037e5955177
SymplyMatt <symplymatt@gmail.com>
2025-05-24 17:22:22 +0100

    Update Loader component background color to #D4AF37 for improved visibility

--- src/components/common/Loader.tsx
+++ src/components/common/Loader.tsx
@@ -2,7 +2,7 @@ import loaderImage from "../../assets/images/logo.svg";
 
 const Loader = () => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
+    <div className="fixed inset-0 flex items-center justify-center bg-[#D4AF37] z-50">
       <img
         src={loaderImage}
         alt="Loading..."
