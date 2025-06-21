6aba9bb9635be5e2eec86fd128554b0ca9a1ae4b
Ajala Matthew Adekunle <symplymatt@gmail.com>
2025-06-21 01:27:02 +0100

    Integrate wishlist functionality into CartProducts component; display product details dynamically and allow adding/removing items from wishlist.

--- src/components/cart/CartProducts.tsx
+++ src/components/cart/CartProducts.tsx
@@ -1,26 +1,26 @@
 import { useDispatch } from "react-redux";
 import { AppDispatch, RootState } from "../../redux/store";
-import { emptyCart } from "../../redux/states/app";
+import { addToWishlist, emptyCart, removeFromWishlist } from "../../redux/states/app";
 import { useSelector } from "react-redux";
 
 const CartProducts = () => {
     const dispatch = useDispatch<AppDispatch>();
-    const { cart } = useSelector((state: RootState) => state.app);
+    const { cart, wishlist } = useSelector((state: RootState) => state.app);
   return (
     <div className="w-full col-span-2 flex flex-col">
         <div className="w-full flex items-center justify-between border-b border-r border-[#D6D6D5] px-[24px] py-[12px]">
             <div className="text-[#141511] text-[20px] font-semibold">YOUR CART</div>
             <div className="flex items-center gap-[12px] text-[#4F4F4D] cursor-pointer font-semibold" onClick={() => dispatch(emptyCart())}><img src="/images/basket.svg"/> DELETE ALL</div>
         </div>
-        <div className="w-full flex flex-col p-[24px] gap-[24px] border-b border-r border-[#D6D6D5]">
-            {cart.map((_, index)=>(
-                <div className="flex items-center justify-between gap-[24px]" key={index}>
+        <div className="w-full flex flex-col p-[24px] gap-[24px] border-b border-r border-[#D6D6D5] h-full">
+            {cart.map((cart, index)=>(
+                <div className="grid grid-cols-[1fr_140px_140px_200px] items-center justify-between gap-[24px] w-full" key={index}>
                     <div className="flex items-center gap-[12px] h-[96px]">
-                        <img src={`/images/cartimage${index + 1}.svg`} className="h-full"/>
+                        <img src={cart.product.images[0].src} className="h-full border border-[#D6D6D5] rounded-[4px]"/>
                         <div className="flex flex-col justify-between h-full py-[8px]">
-                            <div className="text-[#141511] text-[14px] font-semibold whitespace-nowrap">Ocean breeze varsity jacket</div>
+                            <div className="text-[#141511] text-[14px] font-semibold">{cart.product.name}</div>
                             <div className="flex flex-col">
-                                <div className="">Category: Verset jacket</div>
+                                <div className="">Category: {cart.product.categories[0].name}</div>
                                 <div className="">Color: Solid Blue</div>
                             </div>
                         </div>
@@ -35,14 +35,15 @@ const CartProducts = () => {
                         <div className="text-[#676764]">Quantity</div>
                         <div className="flex justify-center items-center gap-[12px]">
                             <img src="/images/prev_cart.svg" className="cursor-pointer"/>
-                            1
+                            {cart.quantity}
                             <img src="/images/next_cart.svg" className="cursor-pointer"/>
                         </div>
                     </div>
                     <div className="flex flex-col gap-[8px] justify-between items-end h-[72px]">
                         <div className="flex items-center gap-[8px] price"> <span className="text-[#8F0024] font-semibold line-through">200.00TND</span><span className="font-semibold">120.00TND</span></div>
                         <div className="flex items-center gap-[12px]">
-                            <img src="/images/heartsm.svg" className="cursor-pointer"/>
+                            {wishlist.some((item) => item.id === cart.product.id) ? <img src="/images/heartfilled.svg" className="cursor-pointer" onClick={()=>dispatch(removeFromWishlist(cart.product.id))}/> : ''}
+                            {!wishlist.some((item) => item.id === cart.product.id) ? <img src="/images/heartsm.svg" className="cursor-pointer" onClick={()=>dispatch(addToWishlist(cart.product))}/> : ''}
                             <img src="/images/basket_sm.svg" className="cursor-pointer"/>
                         </div>
                     </div>
