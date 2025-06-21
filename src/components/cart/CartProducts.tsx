4d2cc06103e671297e16798773741ae6b6cbc5f0
Ajala Matthew Adekunle <symplymatt@gmail.com>
2025-06-21 01:38:04 +0100

    Refactor CartProducts component layout for improved readability and maintainability; ensure dynamic display of product details, discounts, and quantity selection.

--- src/components/cart/CartProducts.tsx
+++ src/components/cart/CartProducts.tsx
@@ -17,42 +17,42 @@ const CartProducts = () => {
                     const discount = (cart.product.regular_price && cart.product.price && cart.product.regular_price > cart.product.price) ? (((Number(cart.product.regular_price) - Number(cart.product.price)) / Number(cart.product.regular_price)) * 100) : 0;
                     const price = Number(cart.product.regular_price);
                     const priceAfterDiscount = Number(cart.product.price);
-                return(
-                    <div className="grid grid-cols-[1fr_140px_140px_200px] items-center justify-between gap-[24px] w-full" key={index}>
-                        <div className="flex items-center gap-[12px] h-[96px]">
-                            <img src={cart.product.images[0].src} className="h-full border border-[#D6D6D5] rounded-[4px]"/>
-                            <div className="flex flex-col justify-between h-full py-[8px]">
-                                <div className="text-[#141511] text-[14px] font-semibold">{cart.product.name}</div>
-                                <div className="flex flex-col">
-                                    <div className="">Category: {cart.product.categories[0].name}</div>
-                                    <div className="">Color: Solid Blue</div>
+                    return(
+                        <div className="grid grid-cols-[1fr_140px_140px_200px] items-center justify-between gap-[24px] w-full" key={index}>
+                            <div className="flex items-center gap-[12px] h-[96px]">
+                                <img src={cart.product.images[0].src} className="h-full border border-[#D6D6D5] rounded-[4px]"/>
+                                <div className="flex flex-col justify-between h-full py-[8px]">
+                                    <div className="text-[#141511] text-[14px] font-semibold">{cart.product.name}</div>
+                                    <div className="flex flex-col">
+                                        <div className="">Category: {cart.product.categories[0].name}</div>
+                                        <div className="">Color: Solid Blue</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="flex flex-col gap-[8px] justify-center">
+                                <div className="text-[#676764]">Product size</div>
+                                <select className="w-[96px] h-[40px] border border-[#D6D6D5] outline-none py-[8px] px-[12px] cursor-pointer">
+                                    <option value="xl">XL</option>
+                                </select>
+                            </div>
+                            <div className="flex flex-col gap-[8px] justify-center">
+                                <div className="text-[#676764]">Quantity</div>
+                                <div className="flex justify-center items-center gap-[12px]">
+                                    <img src="/images/prev_cart.svg" className="cursor-pointer"/>
+                                    {cart.quantity}
+                                    <img src="/images/next_cart.svg" className="cursor-pointer"/>
+                                </div>
+                            </div>
+                            <div className="flex flex-col gap-[8px] justify-between items-end h-[72px]">
+                                <div className="flex items-center gap-[8px] price"> {discount ? <span className="text-[#8F0024] font-semibold line-through">{price}TND</span> : ''}<span className="font-semibold">{priceAfterDiscount}TND</span></div>
+                                <div className="flex items-center gap-[12px]">
+                                    {wishlist.some((item) => item.id === cart.product.id) ? <img src="/images/heartfilled.svg" className="cursor-pointer" onClick={()=>dispatch(removeFromWishlist(cart.product.id))}/> : ''}
+                                    {!wishlist.some((item) => item.id === cart.product.id) ? <img src="/images/heartsm.svg" className="cursor-pointer" onClick={()=>dispatch(addToWishlist(cart.product))}/> : ''}
+                                    <img src="/images/basket_sm.svg" className="cursor-pointer"/>
                                 </div>
                             </div>
                         </div>
-                        <div className="flex flex-col gap-[8px] justify-center">
-                            <div className="text-[#676764]">Product size</div>
-                            <select className="w-[96px] h-[40px] border border-[#D6D6D5] outline-none py-[8px] px-[12px] cursor-pointer">
-                                <option value="xl">XL</option>
-                            </select>
-                        </div>
-                        <div className="flex flex-col gap-[8px] justify-center">
-                            <div className="text-[#676764]">Quantity</div>
-                            <div className="flex justify-center items-center gap-[12px]">
-                                <img src="/images/prev_cart.svg" className="cursor-pointer"/>
-                                {cart.quantity}
-                                <img src="/images/next_cart.svg" className="cursor-pointer"/>
-                            </div>
-                        </div>
-                        <div className="flex flex-col gap-[8px] justify-between items-end h-[72px]">
-                            <div className="flex items-center gap-[8px] price"> {discount ? <span className="text-[#8F0024] font-semibold line-through">{price}TND</span> : ''}<span className="font-semibold">{priceAfterDiscount}TND</span></div>
-                            <div className="flex items-center gap-[12px]">
-                                {wishlist.some((item) => item.id === cart.product.id) ? <img src="/images/heartfilled.svg" className="cursor-pointer" onClick={()=>dispatch(removeFromWishlist(cart.product.id))}/> : ''}
-                                {!wishlist.some((item) => item.id === cart.product.id) ? <img src="/images/heartsm.svg" className="cursor-pointer" onClick={()=>dispatch(addToWishlist(cart.product))}/> : ''}
-                                <img src="/images/basket_sm.svg" className="cursor-pointer"/>
-                            </div>
-                        </div>
-                    </div>
-                )
+                    )
             })}
         </div>
     </div>
