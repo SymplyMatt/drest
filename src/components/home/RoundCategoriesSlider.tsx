7591f6013de04ce0130ff5fe265fba3350863631
Ajala Matthew Adekunle <symplymatt@gmail.com>
2025-07-04 16:26:58 +0100

    Refactor RoundCategoriesSlider component to adjust autoplay settings and improve category display; update category name rendering to truncate long names for better UI.

--- src/components/home/RoundCategoriesSlider.tsx
+++ src/components/home/RoundCategoriesSlider.tsx
@@ -70,7 +70,7 @@ const RoundCategoriesSlider: React.FC<ComponentProp> = ({ handleClick, categorie
                     slidesPerView="auto"
                     loop={true}
                     navigation={true}
-                    autoplay={{ delay: 0, disableOnInteraction: false }}
+                    // autoplay={{ delay: 0, disableOnInteraction: false }}
                     modules={[Autoplay, Navigation]}
                     speed={5000}
                     className="flex items-center justify-between w-full mt-[24px] tmd:mt-[0px]"
@@ -80,22 +80,22 @@ const RoundCategoriesSlider: React.FC<ComponentProp> = ({ handleClick, categorie
                     {splitedCategories.map((category:ProductCategory[], index) => (
                         <SwiperSlide key={index} className="h-full flex flex-col justify-center !w-auto flex-row slide-in-slide">
                             <>
-                                <div className="flex grid-cols-4 items-center">
+                                <div className="flex grid-cols-4 items-center tmd:hidden">
                                     {category.slice(0,4).map((category:ProductCategory, index) =>(
-                                        <div className="w-[25%] relative h-full flex justify-center flex-col items-center gap-[8px] cursor-pointer" key={index}>
+                                        <div className="w-[150px] relative h-full flex justify-center flex-col items-center gap-[8px] cursor-pointer" key={index}>
                                             <img src={category?.image?.src} className="object-cover h-[64px] w-[64px] tmd:w-[100px] tmd:h-[100px] rounded-full" alt="Product" />
                                             <div className="w-full flex text-center items-center whitespace-nowrap justify-center h-[36px] bg-white px-[20px] font-semibold cursor-pointer" onClick={() => {handleClick ? handleClick(category.name) : navigate('/product/productId')}}>
-                                                {category.name.replace(/&amp;/g, "&")}
+                                                {category.name.replace(/&amp;/g, "&").length > 12 ?  category.name.replace(/&amp;/g, "&").substring(0,12) + '...' : category.name.replace(/&amp;/g, "&")}
                                             </div>
                                         </div>
                                     ))}
                                 </div>
                                 <div className="flex grid-cols-4 items-center tmd:hidden">
-                                    {category.slice(4).map((category, index) =>(
-                                        <div className="w-[25%] relative h-full flex justify-center flex-col items-center gap-[8px] cursor-pointer" key={index}>
+                                    {category.slice(4,8).map((category:ProductCategory, index) =>(
+                                        <div className="w-[150px] relative h-full flex justify-center flex-col items-center gap-[8px] cursor-pointer" key={index}>
                                             <img src={category?.image?.src} className="object-cover h-[64px] w-[64px] tmd:w-[100px] tmd:h-[100px] rounded-full" alt="Product" />
                                             <div className="w-full flex text-center items-center whitespace-nowrap justify-center h-[36px] bg-white px-[20px] font-semibold cursor-pointer" onClick={() => {handleClick ? handleClick(category.name) : navigate('/product/productId')}}>
-                                            {category.name.replace(/&amp;/g, "&")}
+                                                {category.name.replace(/&amp;/g, "&").length > 12 ?  category.name.replace(/&amp;/g, "&").substring(0,12) + '...' : category.name.replace(/&amp;/g, "&")}
                                             </div>
                                         </div>
                                     ))}
