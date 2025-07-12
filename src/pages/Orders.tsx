dd26d0c06edba8cb5bc236db5bd46d0fc20ea40f
Ajala Matthew Adekunle <symplymatt@gmail.com>
2025-07-12 19:24:37 +0100

    order list

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,13 +1,16 @@
 import EmptyOrder from "../components/orders/EmptyOrder";
+import OrderList from "../components/orders/OrderList";
 import ProfileNavigation from "../components/orders/ProfileNavigation";
 import Layout from "./Layout";
 
 const Orders = () => {
+    const orders = [1];
     return (
         <Layout headerGap="">
             <div className="w-full grid grid-cols-1 tmd:grid-cols-[360px_1fr] items-center justify-center">
                 <ProfileNavigation />
-                <EmptyOrder />
+                {!orders.length ? <EmptyOrder /> : <></>}
+                {orders.length ? <OrderList /> : <></>}
             </div>
         </Layout>
     );
